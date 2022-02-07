6a710b601373dd27a32faa2e6c177207efc811d7
Parmar Rahul <parmarrahul1974@gmail.com>
2022-02-07 10:32:52 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(null);
+  const [list, setList] = useState([]);
+  const handleChange = (value) => {
+    let num = parseFloat(value);
+    num = Math.round(num * 2) / 2;
+    const arr = [];
+    if (num <= 5) {
+      for (let i = 1; i <= num; i++) {
+        arr.push("full");
+      }
+      if (num % 1 !== 0) {
+        arr.push("half");
+      }
+
+      if (arr.length < 5) {
+        let tmp = 5 - arr.length;
+        for (let i = 1; i <= tmp; i++) {
+          arr.push("empty");
+        }
+      }
+    }
+    setList(arr);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="number" onChange={(e) => handleChange(e.target.value)} />
+      <h1>Rating: {number}</h1>
+
+      <div style={{ display: "flex", flexDirection: "col" }}>
+        {list.map((listValue) => (
+          <div>
+            {listValue === "full" && (
+              <img
+                src="https://pngimg.com/uploads/star/star_PNG1592.png"
+                height="50"
+                width="50"
+              />
+            )}
+            {listValue === "half" && (
+              <img
+                src="https://www.pngfind.com/pngs/m/536-5360626_file-star-half-svg-star-half-hd-png.png"
+                height="50"
+                width="50"
+              />
+            )}
+            {listValue === "empty" && (
+              <img
+                src="https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-star-icon-png-image_4187383.jpg"
+                height="50"
+                width="50"
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
